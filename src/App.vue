81b53807df08acb3efb04e214f9ff0aae8397754
Fauzan <fauzanlutfimuzaki24@gmail.com>
2023-06-08 00:47:18 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+  <Hero />
+</template>
+
+<script>
+import Hero from './components/CompHero.vue'
+import Header from './components/CompHeader.vue'
+export default{
+  name:'App',
+  components:{
+    Hero,
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
